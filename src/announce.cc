6b986a61b7ecbae0516c9325a6382a8b96a17503
Bert JW Regeer <bertjw@regeer.org>
2014-05-12 17:31:14 -0600

    Check if we have enough arguments

--- src/announce.cc
+++ src/announce.cc
@@ -1,8 +1,14 @@
+#include <iostream>
 #include <dns_sd.h>
 #include <ev++.h>
 
+#include "build_version.h"
+
 int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
+    if (2 != argc) {
+        std::cerr << argv[0] << " <announce file>" << std::endl;
+        std::cerr << "Version: " << VERSION << std::endl;
+        return -1;
+    }
     return 0;
 }
