74df3ae5afc6fa65ef48e5be1e8efd583acdaa6a
Bert JW Regeer <bertjw@regeer.org>
2014-05-12 19:27:14 -0600

    Add function to turn a string into a dns string
    DNS strings are uint8_t length, followed by a chunk. The ending should
    be a \0.
    
    So example.com turns into:
    
    <7>example<3>com<0>

---
+++ src/dnsstring.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include "dnsstring.h"
+
+#ifdef DEBUG
+std::string string_to_hex(const std::string& in) {
+    std::stringstream ss;
+
+    ss << std::setw(2) << std::setfill('0');
+    for (unsigned char i : in) {
+        if (i < 32) {
+            ss << std::hex << static_cast<unsigned int>(static_cast<uint8_t>(i));
+        }
+        else {
+            ss << i;
+        }
+
+    }
+
+    return ss.str(); 
+}
+#endif
+
+std::string to_dnsstring(const std::string& s) {
+    std::string _dnsstr;
+    auto begin = s.begin(), end = s.end();
+    auto cur = begin;
+
+    while (cur != end) {
+        cur = std::find(begin, end, '.');
+
+        size_t len = cur - begin;
+        _dnsstr += static_cast<unsigned char>(static_cast<uint8_t>(len));
+        _dnsstr += std::string(begin, cur);
+
+        begin = cur + 1;
+    }
+
+    if (begin < end) {
+        size_t len = end - begin;
+        _dnsstr += static_cast<unsigned char>(static_cast<uint8_t>(len));
+        _dnsstr += std::string(begin, cur);
+    }
+
+    // Append extra \0
+    _dnsstr += static_cast<unsigned char>(static_cast<uint8_t>(0));
+#ifdef DEBUG
+    std::cerr << string_to_hex(_dnsstr.c_str()) << std::endl;
+#endif
+
+    return _dnsstr;
+}
---
+++ src/dnsstring.h
@@ -0,0 +1,9 @@
+#ifndef DNSSTRING_H_47647DDBAF6DE1
+#define DNSSTRING_H_47647DDBAF6DE1
+
+#include <string>
+
+std::string to_dnsstring(const std::string& s);
+
+#endif /* DNSSTRING_H_47647DDBAF6DE1 */
+
