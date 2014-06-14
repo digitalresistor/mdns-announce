3742658be74b1803bcce51a0fb0c7e263a7cc84e
Bert JW Regeer <bertjw@regeer.org>
2014-06-14 00:46:35 -0600

    Add files

---
+++ src/record.h
@@ -0,0 +1,13 @@
+#ifndef RECORD_H_2FCC2B6ED997C7
+#define RECORD_H_2FCC2B6ED997C7
+
+class Record {
+    public:
+        Record();
+        virtual ~Record();
+    private:
+        /* data */
+};
+
+#endif /* RECORD_H_2FCC2B6ED997C7 */
+
